8b4ed73d39fad32c7e16d721e4207ad714da4da5
Eugenio <eucontrerasp@gmail.com>
2020-05-13 17:45:55 -0400

    Productos y categorias

---
+++ server/routes/categoria.js
@@ -0,0 +1,181 @@
+const express = require('express');
+
+let { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion');
+
+let app = express();
+
+let Categoria = require('../models/categoria');
+
+// ////////////////////////////////////
+// Mostrar categorias
+// ////////////////////////////////////
+app.get('/categoria', verificaToken, (req, res) => {
+
+    Categoria.find({})
+        .sort('descripcion')
+        .populate('usuario', 'nombre email')
+        .exec((err, categorias) => {
+
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            };
+
+            res.json({
+                ok: true,
+                categorias
+            });
+
+        });
+
+});
+
+// ////////////////////////////////////
+// Mostrar una categoria por id
+// ////////////////////////////////////
+app.get('/categoria/:id', verificaToken, (req, res) => {
+
+    let id = req.params.id;
+
+    Categoria.findById(id, (err, categoriaDB) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        };
+
+        if (!categoriaDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Categoria no encontrada'
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            categoria: categoriaDB
+        });
+
+    });
+});
+
+// ////////////////////////////////////
+// Crear una nueva categoría
+// ////////////////////////////////////
+app.post('/categoria', verificaToken, (req, res) => {
+    // regresa la nueva categoria
+    // req.usuario._id
+    let body = req.body;
+
+    let categoria = new Categoria({
+        descripcion: body.descripcion,
+        usuario: req.usuario._id
+    });
+
+
+    categoria.save((err, categoriaDB) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!categoriaDB) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            categoria: categoriaDB
+        });
+
+
+    });
+
+
+});
+
+// ////////////////////////////////////
+// Actualizar categoría
+// ////////////////////////////////////
+app.put('/categoria/:id', verificaToken, (req, res) => {
+
+    let id = req.params.id;
+    let body = req.body;
+
+    let descCategoria = {
+        descripcion: body.descripcion
+    }
+
+    Categoria.findByIdAndUpdate(id, descCategoria, { new: true, runValidators: true }, (err, categoriaDB) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!categoriaDB) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            categoria: categoriaDB
+        });
+    });
+
+});
+
+// // ////////////////////////////////////
+// // Eliminar categoría (solo administrador)
+// // ////////////////////////////////////
+app.delete('/categoria/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
+
+    let id = req.params.id;
+
+    Categoria.findByIdAndRemove(id, (err, categoriaBorrada) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        };
+
+        if (!categoriaBorrada) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Id no existe'
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            message: 'Categoria borrada.'
+        });
+
+    });
+
+});
+
+
+
+module.exports = app;
